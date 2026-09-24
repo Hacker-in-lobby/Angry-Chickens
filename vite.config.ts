3f160d9e7bb937a2d54d789e8fd75f380ea8ea60
Hacker-in-lobby <akshay.bussiness.com@gmail.com>
2026-09-24 20:11:08 +0530

    Set base path to /Angry-Chickens/

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
-    base: '/',
+    base: '/Angry-Chickens/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
